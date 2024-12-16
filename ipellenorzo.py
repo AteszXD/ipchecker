4151aa89692341c8f8062f9d4974a01091135d72
AteszXD <144881460+AteszXD@users.noreply.github.com>
2024-12-16 08:46:11 +0100

    megpróbáltuk az ipv6-ot (nem működik)
    Az IPv6 ellenőrző nem működik (Szabi köszi a jó ötleteket mert magamtól nem jutott volna eszembe)

--- ipellenorzo.py
+++ ipellenorzo.py
@@ -2,6 +2,7 @@ import re
 
 ipcim = input("Kérem adjon meg egy ip címet: ")
 ip = re.split(r'[.:]+', ipcim)
+ipv6kar = ['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 1, 2, 3, 4, 5, 6, 7, 8, 9]
 """
 példa IP címek:
 2001:db8:3333:4444:CCCC:DDDD:EEEE:FFFF./192.168.255.255
@@ -17,11 +18,12 @@ def ervenyesitoipv4(ip):
     return False
 
 def ervenyesitoipv6(ip):
-    index = 0
-    for i in range(len(ip)):
-        if ip[index] >= 0 and ip[index] <= 255:
-            index += 1
-    if index == 8:
+    index2 = 0
+    ipjoined = ip.join(':')
+    for karakter in range(len(ipjoined)):
+        if karakter in ipv6kar:
+            index2 += 1
+    if index2 == 8:
         return True
     return False
 
